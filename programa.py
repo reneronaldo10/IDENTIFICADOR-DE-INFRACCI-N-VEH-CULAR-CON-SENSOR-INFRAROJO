3ef05950fd6f03e02228f660c93e77d2853b0b8c
René Gutiérrez <t1013600221@unitru.edu.pe>
2022-08-07 01:09:07 -0500

    volando

--- programa.py
+++ programa.py
@@ -1,7 +1,21 @@
 import serial, time
 
-Serialarduino = serial.Serial('COM4', 9600)
+Serialarduino = serial.Serial('COM1', 9600)
 time.sleep(1)
 
 cad = Serialarduino.readline().decode('ascii')
-if cad = 1:
+
+if cad == 1:
+    import cv2
+    cap = cv2.VideoCapture(0)
+    leido, frame = cap.read()
+    if leido == True:
+        cv2.imwrite("foto.png", frame)
+        print("Foto tomada correctamente")
+    else:
+        print("Error al acceder a la cámara")
+
+elif cad == 0:
+    print("no se ha pasado la linea de pare")
+   
+cap.release()
